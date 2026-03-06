3e8c873c6b507339869b100acb8a7255f425958b
IT SRT Corp <itsrtcorp@ITs-MacBook-Air.local>
2026-03-06 11:01:22 +0700

    fix: add missing Employee import in OrganizationController

--- sobat-api/app/Http/Controllers/Api/OrganizationController.php
+++ sobat-api/app/Http/Controllers/Api/OrganizationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Organization;
+use App\Models\Employee;
 
 class OrganizationController extends Controller
 {
