f3ac003e88e35cb36fd29950f76c2ae132539c1a
IT SRT Corp <itsrtcorp@ITs-MacBook-Air.local>
2026-06-25 11:38:22 +0700

    Fix: Add 'use client' directive to NextUI modal components

--- sobat-web/src/app/employees/components/EmployeeDetailModal.tsx
+++ sobat-web/src/app/employees/components/EmployeeDetailModal.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Chip } from '@nextui-org/react';
 import { Employee } from '../types';
